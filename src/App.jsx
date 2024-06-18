c03eaeededb680388ac77f32eef4a4e41c769dd1
Nishant-Kumar-Sah <nishant.sah2001@gmail.com>
2024-06-18 23:31:19 +0530

    initial layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Chat from './components/chat/Chat'
+import Detail from './components/detail/Detail'
+import List from './components/list/List'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <h1>App</h1>
-        
-    </>
+    <div className="container">
+        <List/>
+        <Chat/>
+        <Detail/>
+    
+    </div>
   )
 }
 
