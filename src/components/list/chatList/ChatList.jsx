60dacf313eec9e5e618fc267e003222ce0b81de9
Nishant-Kumar-Sah <nishant.sah2001@gmail.com>
2024-07-14 22:52:53 +0530

    Chatlist

--- src/components/list/chatList/ChatList.jsx
+++ src/components/list/chatList/ChatList.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import "./chatList.css"
 const ChatList = () => {
-  return (
-    <div className='chatList'>ChatList</div>
-  )
+        return (
+                <div className='chatList'>
+                        <div className="search">
+                                <div className="searchBar">
+                                        <img src="./search.png" alt="" />
+                                        <input type="text" placeholder='Search' name="" id="" />
+                                </div>
+                                <img src="./plus.png" alt="" />
+                        </div>
+                </div>
+        )
 }
 
 export default ChatList
