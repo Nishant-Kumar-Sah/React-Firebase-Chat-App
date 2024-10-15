82da2b61638990c0eec20fa67210b9fb1f630f87
unknown <anishag629@gmail.com>
2024-10-15 23:24:30 +0530

    login

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,24 @@ import { useState } from 'react'
 import Chat from './components/chat/Chat'
 import Detail from './components/detail/Detail'
 import List from './components/list/List'
+import Login from './components/login/Login'
 import './App.css'
 
 const App = () => {
+
+  const user = false
   return (
     <div className="container">
+      {
+        user ? (<>
         <List/>
         <Chat/>
         <Detail/>
+        
+        </>):(<Login/>)
+
+      }
+        
     
     </div>
   )
